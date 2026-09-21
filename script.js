b11da3553da22133d75fd9412b7140516598c0da
LudvigHN <ludvighaukrognielsen@gmail.com>
2026-09-21 13:30:49 +0200

    wrapped generation in function, added reuseablity (if the function is called again it will generate the next pokemons)

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
 import { getIdFromUrl } from "./js/idFromUrl.js";
-let url = "https://pokeapi.co/api/v2/pokemon"
-let listDOM = document.querySelector(".list")
+let apiUrl = "https://pokeapi.co/api/v2/pokemon?limit=60&offset="
+let offset = 0
+const rootDOM = document.querySelector("#root")
 const imgBaseUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"
-fetch(url)
-    .then(response => { return response.json() }
-    )
-    .then(data =>
-        data.results.forEach(element => {
-            const imageUrl = imgBaseUrl+getIdFromUrl(element.url)+".png"
-            const listItem = document.createElement("li")
-            listItem.innerHTML = `
-            <img src="${imageUrl}" alt="${element.name}" class="item_img">
-            `
-            listDOM.append(listItem)
-            
-        }
+function generatePokemons() {
+    const list = document.createElement("ul")
+    list.classList.add("list")
+    fetch(apiUrl+offset)
+        .then(response => { return response.json() }
         )
-    )
+        .then(data =>
+            data.results.forEach(element => {
+                const id = getIdFromUrl(element.url)
+                const imageUrl = imgBaseUrl + id + ".png"
+                const listItem = document.createElement("li")
+                listItem.classList.add("list_item")
+                listItem.innerHTML = `
+                <p class="pokemon_id">#${id}</p>
+                <img src="${imageUrl}" alt="picture of ${element.name}" class="item_img">
+                <p class="pokemon_name">${element.name}</p>
+                `
+
+                list.append(listItem)
+
+            }
+            )
+        )
+        offset += 60
+    return list
+}
+function render() {
+
+    rootDOM.append(generatePokemons())
+}
+function init() {
+
+    render()
+}
+init()
 
 
