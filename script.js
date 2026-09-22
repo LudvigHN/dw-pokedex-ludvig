84894021e4a2d5a387f4fbc56aaa7a55e8664a2a
LudvigHN <ludvighaukrognielsen@gmail.com>
2026-09-22 12:10:48 +0200

    changed ul to be created outside the function so when you try to generate another set of pokemon it dosnt make a new ul

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ let apiUrl = "https://pokeapi.co/api/v2/pokemon?limit=60&"
 const rootDOM = document.querySelector("#root")
 const imgBaseUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"
 const list = document.createElement("ul");
-    list.classList.add("list");
+list.classList.add("list");
 rootDOM.append(list)
-function generateHeader(){
+function generateHeader() {
     const header = document.createElement("header")
     header.classList.add("header")
     header.innerHTML = `
@@ -25,46 +25,49 @@ function generateHeader(){
     return header
 }
 async function generatePokemons() {
-    
+
 
     const response = await fetch(apiUrl);
     const data = await response.json();
 
     apiUrl = data.next;
-                data.results.forEach(element => {
-                const id = getIdFromUrl(element.url)
-                const paddedNumber = id.padStart(3,"0")
-                
-                const imageUrl = imgBaseUrl + id + ".png"
-                const listItem = document.createElement("li")
-                listItem.classList.add("list_item")
-                listItem.innerHTML = `
+    data.results.forEach(element => {
+        const id = getIdFromUrl(element.url)
+        const paddedNumber = id.padStart(3, "0")
+
+        const imageUrl = imgBaseUrl + id + ".png"
+        const listItem = document.createElement("li")
+        listItem.classList.add("list_item")
+        listItem.innerHTML = `
+               
                 <p class="pokemon_id">#${paddedNumber}</p>
                 <img src="${imageUrl}" alt="picture of ${element.name}" class="item_img">
+                <a href="details.html?id=${id}" class="pokemon_link">
                 <p class="pokemon_name">${element.name}</p>
+                </a>
                 `
 
-                list.append(listItem)
-            });
-            
+        list.append(listItem)
+    });
+
 }
 
-function genBtn(){
+function genBtn() {
     const btn = document.createElement("button")
     btn.classList.add("load_more_btn")
     btn.textContent = "load More"
     btn.addEventListener("click", async () => {
         await generatePokemons()
     })
-    
-    
+
+
     return btn
 }
 async function render() {
     rootDOM.append(genBtn())
     rootDOM.prepend(generateHeader())
     await generatePokemons()
-    
+
 }
 function init() {
 
